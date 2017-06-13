5ac850bcde7c09c95aa8be044fd395dbca77d802
Matheus Bernardes <gambler@tutanota.com>
2017-06-13 14:14:34 -0300

    Added client status

--- clientes/models.py
+++ clientes/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 class Clientes(models.Model):
     name = models.CharField(max_length=255, null=False)
@@ -9,3 +10,11 @@ class Clientes(models.Model):
     distro = models.CharField(max_length=255, null=False)
     api = models.CharField(max_length=36,null=False)
     lastupdate = models.DateTimeField(auto_now_add=False,null=True)
+
+    @property
+    def update_status(self,):
+        time = timezone.now() - Clientes.objects.get(id=self.id).lastupdate
+        if time.seconds < 20:
+            return True
+        else:
+            return False
